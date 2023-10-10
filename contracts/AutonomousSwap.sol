1b4ece7f40dfed6f27b7df7612b3c8038922a3e5
Thiago Ribeiro <thiagodeev@gmail.com>
2023-10-09 23:29:55 -0300

    created mappings, structs and started the createOrder function

--- contracts/AutonomousSwap.sol
+++ contracts/AutonomousSwap.sol
@@ -8,23 +8,56 @@ import "@openzeppelin/contracts/token/ERC1155/IERC1155.sol";
 
 contract AutonomousSwap {
 
+  //random number for ERC20, just to identify it
+  bytes4 private constant ERC20_ID = 0xec20ec20;
   bytes4 private constant ERC721_ID = 0x80ac58cd;
   bytes4 private constant ERC1155_ID = 0xd9b67a26;
 
+  mapping (bytes32 orderID => MainOrder) public orders;
+  mapping (address user => mapping (bytes32 orderID => SubOrder)) public orderOf;
 
-  // struct Order {
-  //   address creator;
-  //   address partner;
+  struct MainOrder {
+    address creator;
+    address partner;
+    Status status;
+  }
 
-  // }
+  struct SubOrder {
+    address token;
+    bytes4 tokenType;
+    //can be an quantified ERC20 token or an NFT ID
+    uint256 quantityOrId;
+    //for the case of an ERC1155 token
+    uint256 tokenId;
+    Status individualStatus;
+  }
 
-  // enum Status {
-  //   Pending,
-  //   Submited,
-  //   Accepted,
-  //   Rejected,
-  //   Canceled
-  // }
+  enum Status {
+    Pending,
+    Submited,
+    Accepted,
+    Rejected,
+    Canceled
+  }
+
+  function createOrder(address token, bytes4 interfaceId, uint256 quantityOrId) public {
+    bytes32 randomId = 0xe0d4f6e915eb01068ecd79ce922236bf16c38b2d88cccffcbc57ed53ef3b74aa;
+    orders[randomId].creator = msg.sender;
+
+    SubOrder memory subOrder = SubOrder(
+      token,
+      interfaceId,
+      quantityOrId,
+      0,
+      Status.Submited
+    );
+
+    // MainOrder memory order = MainOrder(
+    //   msg.sender,
+    //   address(0),
+    //   Status.Pending
+    // );
+  }
 
   function supportsInterface(address account, bytes4 interfaceId) external view returns (bool){
     return ERC165Checker.supportsInterface(account, interfaceId);
